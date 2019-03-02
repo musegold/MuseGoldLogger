da53156f5eeb64168aff3d8819f0ded9c7efd0d9
Vinay Ganesh <vganesh@SF-US09457-MBP15.hsd1.ca.comcast.net>
2019-03-02 15:10:46 -0800

    first commit

---
+++ MuseGoldLogger.framework/Headers/MuseGoldLogger.h
@@ -0,0 +1,19 @@
+//
+//  MuseGoldLogger.h
+//  MuseGoldLogger
+//
+//  Created by Vinay Ganesh on 3/2/19.
+//  Copyright © 2019 Vinay Ganesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MuseGoldLogger.
+FOUNDATION_EXPORT double MuseGoldLoggerVersionNumber;
+
+//! Project version string for MuseGoldLogger.
+FOUNDATION_EXPORT const unsigned char MuseGoldLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuseGoldLogger/PublicHeader.h>
+
+
---
+++ MuseGoldLogger/Logger.swift
@@ -0,0 +1,15 @@
+//
+//  Logger.swift
+//  MuseGoldLogger
+//
+//  Created by Vinay Ganesh on 3/2/19.
+//  Copyright © 2019 Vinay Ganesh. All rights reserved.
+//
+
+import Foundation
+
+public class Logger {
+    public func log() {
+        print("Logging")
+    }
+}
